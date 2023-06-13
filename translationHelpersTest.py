f851a6d195f9c8f5b59db4e7fa315c84d643d6d0
Chennamalla <naveen.chennamalla@capgemini.com>
2023-06-13 07:37:40 -0400

    made changes to adapt multiple languages dynamically

---
+++ translationHelpersTest.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+language_codes = {
+    'Haitian Creole': 'hat_Latn',
+    'Hausa': 'hau_Latn',
+    'Hebrew': 'heb_Hebr',
+    'Hindi': 'hin_Deva'
+}
+
+print(language_codes['Hindi'])
